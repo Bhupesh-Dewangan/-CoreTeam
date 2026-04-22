01e84520efc10698544d4d3ce56fbf90073abd52
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-04-22 21:12:18 +0530

    Created UI for Employee Dashboard and Also a Loading Animation for Dashboard

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { use } from 'react'
+import { dummyEmployeeDashboardData } from '../assets/dummyData';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Loading from '../Components/Loading';
+import EmployeeDashboard from '../Components/EmployeeDashboard';
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setData(dummyEmployeeDashboardData);
+    // Simulate fetching data from an API
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if(loading) {
+    return <Loading />;
+  }
+
+  if(!data) {
+    return <p className='text-center text-slate-500 py-12'>Failed to load dashboard data</p>;
+  }
+
+  if(data.role === "Admin") {
+    return <div> Admin Dashboard</div>
+  }
+  else{
+    return <EmployeeDashboard data={data} />;
+  }
+
 }
 
 export default Dashboard
